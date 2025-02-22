9dd7a548549db8199d76d258f3378b470ff96f6a
BlueAndi <web@blue-andi.de>
2025-02-22 20:09:38 +0100

    CustomMarkDownConverter descriptions improved.

--- tools/createTestReport/create_test_report.py
+++ tools/createTestReport/create_test_report.py
@@ -31,7 +31,8 @@ from pyTRLCConverter.trlc_helper import Record_Object
 
 
 class CustomMarkDownConverter(MarkdownConverter):
-    """Custom Project specific Markdown Converter.
+    """Custom Project specific Markdown Converter responsible for converting the
+        SW test case results into Markdown format.
     """
 
     def __init__(self, args: any) -> None:
@@ -48,7 +49,7 @@ class CustomMarkDownConverter(MarkdownConverter):
          Returns:
             str: Converter description
         """
-        return "Convert into project extended markdown format."
+        return "Convert SW test case results to Markdown format."
 
     def convert_section(self, section: str, level: int) -> Ret:
         """Converts a section to Markdown format.
